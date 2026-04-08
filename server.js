8c51c0e0d85d85bbdfcc34a762822dddc80ae5e1
geronimo alvarez <internacionalrep@gmail.com>
2026-04-08 19:57:33 -0300

    Fix: Railway port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const fs      = require('fs');
 const { GoogleAuth } = require('google-auth-library');
 
 const app  = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
